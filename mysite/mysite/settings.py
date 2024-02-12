9caea0f1a7aa5d65e89a2af8169df7239acc84cf
yutao04 <yutakil0414@gmail.com>
2024-02-13 01:37:20 +0900

    言語を日本語に変更、時間帯を東京に

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja' #言語を日本語に変更
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Ais/Tokyo'
 
 USE_I18N = True
 
